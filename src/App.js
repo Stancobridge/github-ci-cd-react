119818eaa3e0e69c89f31b74fd73718f21800ce8
stancobridge <stancoproof@gmail.com>
2022-01-02 22:34:56 +0000

    change to my name

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/stancobridge"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Stanley vercerl App
         </a>
       </header>
     </div>
